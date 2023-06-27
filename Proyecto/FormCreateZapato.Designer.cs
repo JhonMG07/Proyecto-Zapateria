692cad0bfadf53acadbe0f37cecbd123bec61a88
ADRIAN ESTEBAN PAGUAY MONTENEGRO <adrian.paguay@epn.edu.ec>
2023-06-27 08:25:01 -0500

    Form Create zapato completo y Form Update en proceso

---
+++ Proyecto/FormCreateZapato.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Proyecto
+{
+    partial class FormCreateZapato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.labelIdZapato = new System.Windows.Forms.Label();
+            this.labelTipoZapato = new System.Windows.Forms.Label();
+            this.labelTallaZapato = new System.Windows.Forms.Label();
+            this.labelPrecioZapato = new System.Windows.Forms.Label();
+            this.labelDescripcionZapato = new System.Windows.Forms.Label();
+            this.groupBoxInfoZapato = new System.Windows.Forms.GroupBox();
+            this.textBoxIdZapatoCreate = new System.Windows.Forms.TextBox();
+            this.textBoxTipoZapatoCreate = new System.Windows.Forms.TextBox();
+            this.textBoxTallaZapatoCreate = new System.Windows.Forms.TextBox();
+            this.textBoxPrecioZapatoCreate = new System.Windows.Forms.TextBox();
+            this.richTextBoxDescripcionZapatoCreate = new System.Windows.Forms.RichTextBox();
+            this.dataGridViewCreate = new System.Windows.Forms.DataGridView();
+            this.buttonAceptarCreate = new System.Windows.Forms.Button();
+            this.buttonCancelarCreate = new System.Windows.Forms.Button();
+            this.groupBoxInfoZapato.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCreate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelIdZapato
+            // 
+            this.labelIdZapato.AutoSize = true;
+            this.labelIdZapato.Location = new System.Drawing.Point(19, 19);
+            this.labelIdZapato.Name = "labelIdZapato";
+            this.labelIdZapato.Size = new System.Drawing.Size(53, 13);
+            this.labelIdZapato.TabIndex = 0;
+            this.labelIdZapato.Text = "Id Zapato";
+            // 
+            // labelTipoZapato
+            // 
+            this.labelTipoZapato.AutoSize = true;
+            this.labelTipoZapato.Location = new System.Drawing.Point(19, 51);
+            this.labelTipoZapato.Name = "labelTipoZapato";
+            this.labelTipoZapato.Size = new System.Drawing.Size(65, 13);
+            this.labelTipoZapato.TabIndex = 1;
+            this.labelTipoZapato.Text = "Tipo Zapato";
+            // 
+            // labelTallaZapato
+            // 
+            this.labelTallaZapato.AutoSize = true;
+            this.labelTallaZapato.Location = new System.Drawing.Point(19, 82);
+            this.labelTallaZapato.Name = "labelTallaZapato";
+            this.labelTallaZapato.Size = new System.Drawing.Size(67, 13);
+            this.labelTallaZapato.TabIndex = 2;
+            this.labelTallaZapato.Text = "Talla Zapato";
+            // 
+            // labelPrecioZapato
+            // 
+            this.labelPrecioZapato.AutoSize = true;
+            this.labelPrecioZapato.Location = new System.Drawing.Point(19, 113);
+            this.labelPrecioZapato.Name = "labelPrecioZapato";
+            this.labelPrecioZapato.Size = new System.Drawing.Size(74, 13);
+            this.labelPrecioZapato.TabIndex = 3;
+            this.labelPrecioZapato.Text = "Precio Zapato";
+            // 
+            // labelDescripcionZapato
+            // 
+            this.labelDescripcionZapato.AutoSize = true;
+            this.labelDescripcionZapato.Location = new System.Drawing.Point(19, 144);
+            this.labelDescripcionZapato.Name = "labelDescripcionZapato";
+            this.labelDescripcionZapato.Size = new System.Drawing.Size(100, 13);
+            this.labelDescripcionZapato.TabIndex = 4;
+            this.labelDescripcionZapato.Text = "Descripción Zapato";
+            // 
+            // groupBoxInfoZapato
+            // 
+            this.groupBoxInfoZapato.Controls.Add(this.richTextBoxDescripcionZapatoCreate);
+            this.groupBoxInfoZapato.Controls.Add(this.textBoxPrecioZapatoCreate);
+            this.groupBoxInfoZapato.Controls.Add(this.textBoxTallaZapatoCreate);
+            this.groupBoxInfoZapato.Controls.Add(this.textBoxTipoZapatoCreate);
+            this.groupBoxInfoZapato.Controls.Add(this.textBoxIdZapatoCreate);
+            this.groupBoxInfoZapato.Controls.Add(this.labelIdZapato);
+            this.groupBoxInfoZapato.Controls.Add(this.labelDescripcionZapato);
+            this.groupBoxInfoZapato.Controls.Add(this.labelTipoZapato);
+            this.groupBoxInfoZapato.Controls.Add(this.labelPrecioZapato);
+            this.groupBoxInfoZapato.Controls.Add(this.labelTallaZapato);
+            this.groupBoxInfoZapato.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxInfoZapato.Name = "groupBoxInfoZapato";
+            this.groupBoxInfoZapato.Size = new System.Drawing.Size(339, 301);
+            this.groupBoxInfoZapato.TabIndex = 6;
+            this.groupBoxInfoZapato.TabStop = false;
+            this.groupBoxInfoZapato.Text = "Info Zapato";
+            // 
+            // textBoxIdZapatoCreate
+            // 
+            this.textBoxIdZapatoCreate.Location = new System.Drawing.Point(78, 16);
+            this.textBoxIdZapatoCreate.Name = "textBoxIdZapatoCreate";
+            this.textBoxIdZapatoCreate.Size = new System.Drawing.Size(232, 20);
+            this.textBoxIdZapatoCreate.TabIndex = 5;
+            // 
+            // textBoxTipoZapatoCreate
+            // 
+            this.textBoxTipoZapatoCreate.Location = new System.Drawing.Point(90, 48);
+            this.textBoxTipoZapatoCreate.Name = "textBoxTipoZapatoCreate";
+            this.textBoxTipoZapatoCreate.Size = new System.Drawing.Size(220, 20);
+            this.textBoxTipoZapatoCreate.TabIndex = 6;
+            // 
+            // textBoxTallaZapatoCreate
+            // 
+            this.textBoxTallaZapatoCreate.Location = new System.Drawing.Point(92, 79);
+            this.textBoxTallaZapatoCreate.Name = "textBoxTallaZapatoCreate";
+            this.textBoxTallaZapatoCreate.Size = new System.Drawing.Size(219, 20);
+            this.textBoxTallaZapatoCreate.TabIndex = 7;
+            // 
+            // textBoxPrecioZapatoCreate
+            // 
+            this.textBoxPrecioZapatoCreate.Location = new System.Drawing.Point(99, 110);
+            this.textBoxPrecioZapatoCreate.Name = "textBoxPrecioZapatoCreate";
+            this.textBoxPrecioZapatoCreate.Size = new System.Drawing.Size(212, 20);
+            this.textBoxPrecioZapatoCreate.TabIndex = 8;
+            // 
+            // richTextBoxDescripcionZapatoCreate
+            // 
+            this.richTextBoxDescripcionZapatoCreate.Location = new System.Drawing.Point(22, 160);
+            this.richTextBoxDescripcionZapatoCreate.Name = "richTextBoxDescripcionZapatoCreate";
+            this.richTextBoxDescripcionZapatoCreate.Size = new System.Drawing.Size(288, 128);
+            this.richTextBoxDescripcionZapatoCreate.TabIndex = 10;
+            this.richTextBoxDescripcionZapatoCreate.Text = "";
+            // 
+            // dataGridViewCreate
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewCreate.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewCreate.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewCreate.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewCreate.Location = new System.Drawing.Point(357, 12);
+            this.dataGridViewCreate.Name = "dataGridViewCreate";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewCreate.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewCreate.Size = new System.Drawing.Size(431, 426);
+            this.dataGridViewCreate.TabIndex = 7;
+            // 
+            // buttonAceptarCreate
+            // 
+            this.buttonAceptarCreate.Location = new System.Drawing.Point(34, 372);
+            this.buttonAceptarCreate.Name = "buttonAceptarCreate";
+            this.buttonAceptarCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptarCreate.TabIndex = 8;
+            this.buttonAceptarCreate.Text = "Aceptar";
+            this.buttonAceptarCreate.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelarCreate
+            // 
+            this.buttonCancelarCreate.ForeColor = System.Drawing.Color.Red;
+            this.buttonCancelarCreate.Location = new System.Drawing.Point(247, 372);
+            this.buttonCancelarCreate.Name = "buttonCancelarCreate";
+            this.buttonCancelarCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelarCreate.TabIndex = 9;
+            this.buttonCancelarCreate.Text = "Cancelar";
+            this.buttonCancelarCreate.UseVisualStyleBackColor = true;
+            // 
+            // FormCreateZapato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCancelarCreate);
+            this.Controls.Add(this.buttonAceptarCreate);
+            this.Controls.Add(this.dataGridViewCreate);
+            this.Controls.Add(this.groupBoxInfoZapato);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormCreateZapato";
+            this.Text = "Crear Zapato";
+            this.groupBoxInfoZapato.ResumeLayout(false);
+            this.groupBoxInfoZapato.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCreate)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelIdZapato;
+        private System.Windows.Forms.Label labelTipoZapato;
+        private System.Windows.Forms.Label labelTallaZapato;
+        private System.Windows.Forms.Label labelPrecioZapato;
+        private System.Windows.Forms.Label labelDescripcionZapato;
+        private System.Windows.Forms.GroupBox groupBoxInfoZapato;
+        private System.Windows.Forms.RichTextBox richTextBoxDescripcionZapatoCreate;
+        private System.Windows.Forms.TextBox textBoxPrecioZapatoCreate;
+        private System.Windows.Forms.TextBox textBoxTallaZapatoCreate;
+        private System.Windows.Forms.TextBox textBoxTipoZapatoCreate;
+        private System.Windows.Forms.TextBox textBoxIdZapatoCreate;
+        private System.Windows.Forms.DataGridView dataGridViewCreate;
+        private System.Windows.Forms.Button buttonAceptarCreate;
+        private System.Windows.Forms.Button buttonCancelarCreate;
+    }
+}
