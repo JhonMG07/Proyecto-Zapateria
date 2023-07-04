c6300a2fc13e8594259daa09a0d84fdc51a73391
estevan-j <esteban.bajana@epn.edu.ec>
2023-07-04 13:44:14 -0500

    Rectificación DELETE.

--- Proyecto/FormDelete.cs
+++ Proyecto/FormDelete.cs
@@ -13,13 +13,20 @@ namespace Proyecto
 {
     public partial class FormDelete : Form
     {
+      
+        List<Zapato> zapatos;
         private BindingSource bindingSource;
+        string cadenaConexion;
         public FormDelete()
         {
             InitializeComponent();
+            cadenaConexion = "Data Source=Zapatería.db;Version=3;";
             this.StartPosition = FormStartPosition.CenterScreen;
             bindingSource = new BindingSource();
             dataGridView1.DataSource = bindingSource;
+            zapatos = new List<Zapato>();
+            CargarZapatosDesdeBD();
+
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
@@ -32,9 +39,8 @@ namespace Proyecto
             int zapatoId = Convert.ToInt32(txt_id.Text); // Reemplaza esto con tu lógica para obtener el ID
 
             // Crea una instancia del contexto de la base de datos
-            string connectionString = "Data Source=Zapatería.db;Version=3;"; // Establece la cadena de conexión a tu base de datos
 
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            using (SQLiteConnection connection = new SQLiteConnection(cadenaConexion))
             {
                 connection.Open();
 
@@ -45,22 +51,92 @@ namespace Proyecto
                     command.Parameters.AddWithValue("@IdZapato", zapatoId);
                     DialogResult result = MessageBox.Show($"¿Está seguro de eliminar el zapato ID: {zapatoId}?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                     
-                    if (result == DialogResult.Yes)
+                    if (result == DialogResult.Yes && BusquedaBinarioZapato(zapatoId))
                     {
+                        dataGridView1.DataSource = null;
                         int rowsAffected = command.ExecuteNonQuery();
                         MessageBox.Show("Zapato eliminado exitosamente.");
+                        dataGridView1.DataSource = zapatos;
                     }
                     else
                     {
                         MessageBox.Show("No se encontró un zapato con el ID especificado.");
                     }
                 }
-
+                
                 connection.Close();
             }
 
         }
 
+        private bool BusquedaBinarioZapato(int targetId)
+        {
+            int left = 0;
+            int right = zapatos.Count - 1;
+
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+
+                if (zapatos[middle].Id_zapato == targetId)
+                {
+                    zapatos.RemoveAt(middle);
+                    return true;
+                }
+                else if (zapatos[middle].Id_zapato < targetId)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            return false; // Zapato no encontrado
+        }
+        private void CargarZapatosDesdeBD()
+        {
+            try
+            {
+                SQLiteConnection conexion = new SQLiteConnection(cadenaConexion);
+                // Abrir la conexión a la base de datos
+                conexion.Open();
+
+                // Consulta SQL para obtener los zapatos
+                string consulta = "SELECT * FROM Zapato";
+
+                // Crear el comando SQL
+                SQLiteCommand comando = new SQLiteCommand(consulta, conexion);
+
+                // Ejecutar el comando y obtener un lector de datos
+                SQLiteDataReader lector = comando.ExecuteReader();
+
+                // Recorrer los resultados y agregarlos a la lista de zapatos
+                while (lector.Read())
+                {
+                    int id = Convert.ToInt32(lector["id_zapato"]);
+                    string tipo = lector["tipo"].ToString();
+                    int talla = Convert.ToInt32(lector["talla"]);
+                    float precio = Convert.ToSingle(lector["precio"]);
+                    string descripcion = lector["descripcion"].ToString();
+                    string genero = lector["genero"].ToString();
+
+                    Zapato zapato = new Zapato(id, tipo, talla, precio, descripcion, genero);
+                    zapatos.Add(zapato);
+                }
+
+                // Cerrar el lector de datos
+                lector.Close();
+
+                // Cerrar la conexión a la base de datos
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             CRUD backCrud = new CRUD();
